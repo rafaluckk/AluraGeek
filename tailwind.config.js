de3d9378f38a45758c5169dec1d3f4e68d04823f
rafaluckk <rafael2021516@gmail.com>
2022-10-14 13:51:38 -0300

    footer

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      margin: {
+        "700px": "700px",
+      },
+    },
+  },
+  plugins: [],
+};
